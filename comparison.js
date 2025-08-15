ec015185c6df7d25ad3da16aeec6ad2253824f26
SwadebDeb sharma <swadebriva2012@gmail.com>
2025-08-15 19:38:44 +0600

    learning js comparison and conditions

---
+++ comparison.js
@@ -0,0 +1,16 @@
+console.log(5 > 10)   // False
+console.log(15 > 10)   // true
+
+console.log(5 < 10) // true
+console.log(15 < 10)  // false
+
+console.log(5 == 10)  // false
+console.log(10 == 10)  // true
+
+console.log( 10 >= 10)  // true
+console.log( 15 >= 10)  // true
+console.log(5 >= 10)   // false
+
+
+console.log(5 <= 10) // true
+console.log(10 <= 10)  // true
