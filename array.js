ece0e5594e8b5d6014dcef53bb877d51d8d79084
SwadebDeb sharma <swadebriva2012@gmail.com>
2025-08-16 23:47:22 +0600

    array discussion

---
+++ array.js
@@ -0,0 +1,21 @@
+// const numbers = [1, 2, 3, 4, 5, 6 , 7, 8 , 9, 10];
+// console.log(numbers.length, numbers);
+
+const friends = ['Abul', 'Babul', 'Kabul', 'Sabul', 'Dabul', "Malu", 'Khalu'];
+
+const myFriends = friends.length;
+// console.log(myFriends)
+
+const numbers = [12, 22, 32, 42, 52, 62 , 72, 82 , 92, 10];
+
+// Set Value by Index
+numbers[3] = 420;
+
+// Find out the 3rd index value
+const thirdItem = numbers[3];
+
+
+
+
+console.log(thirdItem);
+console.log(numbers)
